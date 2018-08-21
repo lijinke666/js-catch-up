e8c755ed5447cbbe35a020526d5075e128d7af5f
Jinke.Li <1359518268@qq.com>
2018-08-21 11:13:40 +0800

    fix

--- react/src/app.js
+++ react/src/app.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
-class App extends Component {
+import "./styles.css"
+
+@hot(module)
+export default class App extends Component {
   goto = url => {
     this.props.history.push(url);
   };
   render() {
     return (
       <>
-        <button onClick={() => this.goto('/footer')}>高阶组件基本用法</button>
+        <button onClick={() => this.goto('/footer')}>(组件包裹)</button>
         <button onClick={() => this.goto('/autoLoading')}>
-          反向继承 (自动显示加载 autoLoading)
+          反向继承 和渲染劫持 (自动显示加载 autoLoading)
         </button>
         <button onClick={() => this.goto('/connect')}>
           属性代理 (Connect)
         </button>
-        <button onClick={()=> this.goto('/bind')}>双向绑定</button>
+        <button onClick={()=> this.goto('/bind')}>更改 Props (双向绑定)</button>
       </>
     );
   }
 }
-
-export default hot(module)(App);
